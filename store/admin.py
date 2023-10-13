528607672dd7a960702d8a494753d25a8fd8bbb3
gustavo lora <tavoalp1999@gmail.com>
2023-10-12 21:09:10 -0500

    changes

--- store/admin.py
+++ store/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from store.models import NewUser
+
+from .models import NewUser
+
+admin.site.register(NewUser, UserAdmin)
--- store/views.py
+++ store/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 
-# Create your views here.
+def login(request):
+    if request.method=='POST':
+        user = request.POST['name']
+        
