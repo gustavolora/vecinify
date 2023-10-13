b4b12f4d6ad5f2ee66c5c60a2ea305acb4c32d8e
jhon <eduardcamayo13@gmail.com>
2023-10-12 21:17:55 -0500

    changes

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
