c89d117f77a97621cf6bc9a5a5997b8f8eb7075a
gustavo lora <tavoalp1999@gmail.com>
2023-10-20 00:24:30 -0500

    changing password validator min len 8

--- proyectofinal/settings.py
+++ proyectofinal/settings.py
@@ -99,9 +99,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        'OPTIONS': {
-            'min_length': 8,
-        },
+        
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
