c5b2bec22b881cea50becc05f07286af4b5550e4
gustavo lora <tavoalp1999@gmail.com>
2023-10-22 04:12:57 -0500

    adding major changes

--- proyectofinal/urls.py
+++ proyectofinal/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.conf.urls import handler500, handler404, handler403
+from store.views import Handler500view, handler404notfound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('store.urls')),
 
 ]
+
+handler400 = handler404notfound
+handler500 = Handler500view.as_error_view()
